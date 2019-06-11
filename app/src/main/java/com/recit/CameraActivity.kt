5ad71f354fe430efdb1a3c2aa368f0ee1c81bff4
GinPachi <m.fawaadali98@gmail.com>
2019-06-11 19:13:04 +0500

    Update R2

--- app/src/main/java/com/recit/CameraActivity.kt
+++ app/src/main/java/com/recit/CameraActivity.kt
@@ -1,53 +1,70 @@
 package com.recit
 
-import android.app.Activity
-import android.content.ContentValues.TAG
+import android.Manifest
+import android.content.ContentValues
 import android.content.Context
 import android.content.pm.PackageManager
 import android.hardware.Camera
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.SurfaceHolder
-import android.view.SurfaceView
 import android.widget.FrameLayout
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_camera.*
-import kotlinx.android.synthetic.main.activity_camera.view.*
+import android.graphics.*
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import java.io.ByteArrayOutputStream
+import kotlinx.coroutines.*
 import java.io.IOException
-import android.R.attr.bitmap
-import android.content.res.AssetManager
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.ImageFormat
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
-import java.lang.IllegalStateException
+import android.view.*
 
 
-class CameraActivity : Activity() {
-
+class CameraActivity : AppCompatActivity() {
     private var mCamera: Camera? = null
     private var mPreview: CameraPreview? = null
+
+    var isProcessingF = false
+    val CAMERA_BACK = 0
+    val CAMERA_FRONT = 1
+    var cameraId = CAMERA_BACK
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
-        mCamera = getCameraInstance()
-        mPreview = mCamera?.let {
-            CameraPreview(this, it, assets, cameraResult)
-        }
+        checkPermission()
+
+        mCamera = getCameraInstance(cameraId)
+        setCameraPreview()
         if(!baseContext.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)){
             cameraResult.text = "Unable to use camera"
         }
 
-        mPreview.also {
-            val preview: FrameLayout = cameraPreview
-            preview.addView(it)
+        cameraChange.setOnClickListener{
+            if(cameraId == CAMERA_FRONT){
+                cameraId = CAMERA_BACK
+                mCamera?.release()
+                mCamera = getCameraInstance(cameraId)
+                setCameraPreview()
+            } else if(cameraId == CAMERA_BACK){
+                cameraId = CAMERA_FRONT
+                mCamera?.release()
+                mCamera = getCameraInstance(cameraId)
+                setCameraPreview()
+            }
         }
 
+        val classifier = ImageClassifier(assets)
+
+        mCamera?.setPreviewCallback{data, camera->
+            onPreviewCallBack(data, camera, classifier)
+        }
+    }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
+        }
     }
 
     override fun onPause() {
@@ -55,98 +72,127 @@ class CameraActivity : Activity() {
         mCamera?.release() // release the camera immediately on pause event
     }
 
-    fun getCameraInstance(): Camera? {
+    override fun onResume() {
+        super.onResume()
+        mCamera = getCameraInstance(cameraId)
+        setCameraPreview()
+    }
+
+    fun getCameraInstance(cameraId: Int): Camera? {
+        if(mCamera != null){
+            mCamera?.release()
+        }
         return try {
-            Camera.open() // attempt to get a Camera instance
+            Camera.open(cameraId) // attempt to get a Camera instance
         } catch (e: Exception) {
             // Camera is not available (in use or does not exist)
             null // returns null if camera is unavailable
         }
     }
 
-    class CameraPreview(context: Context, private val mCamera: Camera, assets: AssetManager, cameraResult: TextView) : SurfaceView(context), SurfaceHolder.Callback, Camera.PreviewCallback
-    {
-
-        var isProcessingF = false
+    fun setCameraPreview(){
+        mPreview = mCamera?.let {
+            CameraPreview(this, it)
+        }
+        mPreview.also {
+            val preview: FrameLayout = cameraPreview
+            preview.addView(it, 0)
+        }
         val classifier = ImageClassifier(assets)
-        var imageFormat: Int? = null
+        mCamera?.setPreviewCallback{data, camera->
+            onPreviewCallBack(data, camera, classifier)
+        }
+    }
 
-        override fun onPreviewFrame(data: ByteArray, camera: Camera?) {
-            if(imageFormat == ImageFormat.NV21)
-                if(isProcessingF){
-                    Log.i("Skipping Frame", "Skipping frame because of processing")
-                    return
-                }else{
-                    Log.i("STEP 1", data.toString())
-                    isProcessingF = true
-                    try{
-                        Log.i("STEP 2", data.inputStream().toString())
-                        Log.i("STEP 3", BitmapFactory.decodeStream(data.inputStream()).toString())
-//                        val bitMap = BitmapFactory.decodeStream(data.inputStream())
-//                        val resultArr = classifier.recognizeImage(bitMap)
-//                        Log.i("IMAGE DATA", data.toString())
-                        //cameraResult.text = resultArr.joinToString("\n")
-                    }catch (e: IllegalStateException){
-                        Log.i("Exception", e.toString())
+    fun onPreviewCallBack(data: ByteArray, camera: Camera, classifier: ImageClassifier){
+        Log.i("INSIDE", "INSIDE PREVIEW CALLBACK")
+        if(!isProcessingF){
+            isProcessingF = true
+
+            GlobalScope.launch {
+                val parameters = camera!!.parameters
+                val width = parameters.previewSize.width
+                val height = parameters.previewSize.height
+
+                val yuv = YuvImage(data, parameters.previewFormat, width, height, null)
+
+                val out = ByteArrayOutputStream()
+                yuv.compressToJpeg(Rect(0, 0, Keys.INPUT_SIZE, Keys.INPUT_SIZE), 10, out)
+
+                val bytes = out.toByteArray()
+                try{
+                    val bitMap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                    val resultArr = classifier.recognizeImage(bitMap)
+                    Log.i("RESULT", resultArr.joinToString ("\n"))
+                    this@CameraActivity.runOnUiThread{
+                        cameraResult.text = resultArr[0].toString()
                     }
-                    isProcessingF = false
+
+                }catch (e: Exception){
+                    Log.e("Exception", e.toString())
                 }
-        }
-
-        private val mHolder: SurfaceHolder = holder.apply {
-            // Install a SurfaceHolder.Callback so we get notified when the
-            // underlying surface is created and destroyed.
-            addCallback(this@CameraPreview)
-            // deprecated setting, but required on Android versions prior to 3.0
-            setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
-        }
-
-        override fun surfaceCreated(holder: SurfaceHolder) {
-            // The Surface has been created, now tell the camera where to draw the preview.
-            mCamera.apply {
-                try {
-                    setPreviewDisplay(holder)
-                    imageFormat = this.parameters.previewFormat
-                    startPreview()
-                } catch (e: IOException) {
-                    Log.d(TAG, "Error setting camera preview: ${e.message}")
-                }
-            }
-        }
-
-        override fun surfaceDestroyed(holder: SurfaceHolder) {
-            // empty. Take care of releasing the Camera preview in your activity.
-        }
-
-        override fun surfaceChanged(holder: SurfaceHolder, format: Int, w: Int, h: Int) {
-            // If your preview can change or rotate, take care of those events here.
-            // Make sure to stop the preview before resizing or reformatting it.
-            if (mHolder.surface == null) {
-                // preview surface does not exist
-                return
-            }
-
-            // stop preview before making changes
-            try {
-                mCamera.stopPreview()
-                mCamera.setPreviewCallback(this);
-            } catch (e: Exception) {
-                // ignore: tried to stop a non-existent preview
-            }
-
-            // set preview size and make any resize, rotate or
-            // reformatting changes here
-
-            // start preview with new settings
-            mCamera.apply {
-                try {
-                    setPreviewDisplay(mHolder)
-                    startPreview()
-                } catch (e: Exception) {
-                    Log.d(TAG, "Error starting camera preview: ${e.message}")
-                }
+                isProcessingF = false
             }
         }
     }
 
+}
+
+class CameraPreview(context: Context, private val mCamera: Camera) : SurfaceView(context), SurfaceHolder.Callback
+{
+
+    private val mHolder: SurfaceHolder = holder.apply {
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        addCallback(this@CameraPreview)
+        // deprecated setting, but required on Android versions prior to 3.0
+        setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, w: Int, h: Int) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+        mCamera.apply {
+            try {
+                mCamera.setDisplayOrientation(90)
+                setPreviewDisplay(holder)
+                startPreview()
+            } catch (e: IOException) {
+                Log.d(ContentValues.TAG, "Error setting camera preview: ${e.message}")
+            }
+        }
+        if (mHolder.surface == null) {
+            // preview surface does not exist
+            return
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview()
+        } catch (e: Exception) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        mCamera.apply {
+            try {
+                setPreviewDisplay(mHolder)
+                startPreview()
+            } catch (e: Exception) {
+                Log.d(ContentValues.TAG, "Error starting camera preview: ${e.message}")
+            }
+        }
+    }
 }
